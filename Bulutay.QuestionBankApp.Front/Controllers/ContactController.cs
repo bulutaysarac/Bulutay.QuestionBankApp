738b7272233f8af106db9a300ac6c984684beb70
bulutaysarac <bulutaysarac@gmail.com>
2023-04-20 15:08:50 +0300

    Update 20.04.2023

---
+++ Bulutay.QuestionBankApp.Front/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bulutay.QuestionBankApp.Front.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bulutay.QuestionBankApp.Front/Controllers/ExamplesController.cs
@@ -0,0 +1,28 @@
+﻿using Bulutay.QuestionBankApp.Front.HttpRequestHelpers;
+using Bulutay.QuestionBankApp.Front.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Bulutay.QuestionBankApp.Front.Controllers
+{
+    public class ExamplesController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ExamplesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var questions = await QBRequests.GetAllAsync<QuestionListModel>(_httpClientFactory, QBApiLinks.QUESTIONS, "");
+            return questions == null ? View() : View(questions);
+        }
+    }
+}
